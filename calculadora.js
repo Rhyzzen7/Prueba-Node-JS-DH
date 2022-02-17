34249d0c47b488304edbdb148495c5bc0c6fe29e
Rhyzzen7 <thiago_8_ferraro@hotmail.com>
2022-02-17 12:33:57 -0300

    calculadora realizada completamente

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+const sumar = require ("./sumar");
+const restar = require ("./restar");
+const multiplicar = require ("./multiplicar");
+const dividir = require ("./dividir");
+
+console.log(sumar(500,100));
+console.log(restar(100,10));
+console.log(multiplicar(12,10));
+console.log(dividir(0,19));
