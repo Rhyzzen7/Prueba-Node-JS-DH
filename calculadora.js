750ea162524ab95addd3682fe36584e9d6bf100e
Rhyzzen7 <thiago_8_ferraro@hotmail.com>
2022-02-17 19:38:21 -0300

    agregue mas console.logs de operaciones

--- calculadora.js
+++ calculadora.js
@@ -8,3 +8,8 @@ console.log(restar(100,10));
 console.log(multiplicar(12,10));
 console.log(dividir(25,4));
 console.log(dividir(30,0));
+console.log(sumar(1000,100));
+console.log(restar(500,99));
+console.log(multiplicar(10,50));
+console.log(dividir(30,6));
+console.log(dividir(30,0));
